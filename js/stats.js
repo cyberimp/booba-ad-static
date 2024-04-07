480353af83777232c345cccb9c01523edf07b558
Andrey Kalikin <kinzo.x.beato@gmail.com>
2024-04-07 14:49:53 +0300

    horny users

--- js/stats.js
+++ js/stats.js
@@ -3,6 +3,7 @@
  * @type {Object}
  * @property {string} date
  * @property {number} requests
+ * @property {number} users
  */
 
 const ctx = document.getElementById('myChart');
@@ -17,19 +18,30 @@ fetch('https://hentai.tiddies.pics/dateChart.json')
     data: {
       labels: data.map(row => row.date),
       datasets: [{
-        label: 'horny requests',
-        data: data.map(row => row.requests),
-        borderWidth: 1,
-        backgroundColor: 'rgba(255, 99, 132, 0.2)',
-        borderColor: 'rgb(255, 99, 132)'
-
-      }]
+          label: 'horny requests',
+          data: data.map(row => row.requests),
+          borderWidth: 1,
+          backgroundColor: 'rgba(255, 99, 132, 0.2)',
+          borderColor: 'rgb(255, 99, 132)'
+        },
+        {
+          label: 'horny users',
+          data: data.map(row => row.users),
+          borderWidth: 1,
+          backgroundColor: 'rgba(132, 99, 255, 0.2)',
+          borderColor: 'rgb(132, 99, 255)'
+        },
+      ]
     },
     options: {
       maintainAspectRatio: false,
       scales: {
         y: {
-          beginAtZero: true
+          beginAtZero: true,
+          stacked: false
+        },
+        x: {
+          stacked: true
         }
       }
     }
